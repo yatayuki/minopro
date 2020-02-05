7132aacb51bc04f810daa4816ab2629bd32e67a8
矢田悠輝 <yatayuki@yatayuuakiranoMacBook.local>
2020-02-05 23:41:44 +0900

    get_contentful_API

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 import colors from 'vuetify/es5/util/colors'
-
+require('dotenv').config()
 export default {
   mode: 'universal',
   /*
@@ -30,6 +30,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    'plugins/contentful'
   ],
   /*
   ** Nuxt.js dev-modules
@@ -44,12 +45,19 @@ export default {
   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/dotenv'
   ],
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
+  env: {
+    // contentful
+    CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+    CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID,
+    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN
+  },
   axios: {
   },
   /*
